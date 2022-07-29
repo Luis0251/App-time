0b4bfb01199ef42b4478d94183c2feee4c8943de
Luis0251 <luis0251@hotmail.com>
2022-07-29 12:57:46 -0500

    components and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchModule } from './components/search/search.module';
+import { WeatherModule } from './pages/weather/weather.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SearchModule,
+    WeatherModule
   ],
   providers: [],
   bootstrap: [AppComponent]
