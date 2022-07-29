f0c0fe4ace6c6f0fa755c90ee75a0408edd9e6d0
Luis0251 <luis0251@hotmail.com>
2022-07-29 16:08:47 -0500

    Import weather component

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { WeatherData } from 'src/app/shared/interface/interface.weather';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WeatherComponent implements OnInit {
-
+  @Input() public weather!:WeatherData
   constructor() { }
 
   ngOnInit(): void {
